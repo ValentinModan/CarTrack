4c0fffb90eae8620a8438166d77d9194f11cc970
ValentinModan <valentinmodan@yahoo.com>
2019-08-27 23:38:42 +0300

    Second

--- Cod.cpp
+++ Cod.cpp
@@ -3,13 +3,23 @@
 
 using namespace std;
 
+typedef struct coordonate {
+	int grade;
+	int minute;
+	int secunde;
+}coordonate;
+
+typedef struct automobil{
+
+}automobil;
+
 int main()
 {
 	int x;
 
 	freopen("test.in","r",stdin);
 	freopen("test.out","w",stdout);
-	
+
 	printf("Afiseaza un numar");
 	scanf("%d",&x);
 
