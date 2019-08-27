b57ab5f83f4d5620a6fb5efa0a463f369430e9a1
ValentinModan <valentinmodan@yahoo.com>
2019-08-27 23:32:40 +0300

    First commiiiiit

---
+++ Cod.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+
+	freopen("test.in","r",stdin);
+	freopen("test.out","w",stdout);
+	
+	printf("Afiseaza un numar");
+	scanf("%d",&x);
+
+	printf("%d\n",x);
+	printf("Afiseaza un numar");
+
+	
+	
+}
